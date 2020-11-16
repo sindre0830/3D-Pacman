29d8386196beca9f4839d97d10aec4d1458e2a3a
Sindre Eiklid <sindre0830@gmail.com>
2020-11-16 15:57:12 +0100

    Formatting

--- camera.cpp
+++ camera.cpp
@@ -2,50 +2,48 @@
 #include "header/camera.h"
 #include "header/dictionary.h"
 #include "header/levelData.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 extern LevelData *g_level;
 
 Camera::~Camera() {}
 
 Camera::Camera(const int width, const int height) {
-	//setup projection matrix
-	fov = 60.f;
-	nearPlane = 0.01f;
-	farPlane = 100.f;
-	projectionMatrix = glm::mat4(1.f);
-	projectionMatrix = glm::perspective(glm::radians(fov), static_cast<float>(width) / height, nearPlane, farPlane);
-    //setup view matrix
+	//set projection matrix
+	projectionMatrix = glm::perspective(glm::radians(60.f), static_cast<float>(width) / height, 0.01f, 100.f);
+    //set view matrix values
 	camPos = glm::vec3(0.f, 0.f, 0.f);
 	camFront = glm::vec3(0.f, 0.f, -1.f);
 	camUp = glm::vec3(0.f, 1.f, 0.f);
 	//set initial direction
 	changeDirection(UP);
-
-	viewMatrix = glm::mat4(1.f);
+	//set view matrix
 	viewMatrix = glm::lookAt(camPos, camPos + camFront, camUp);
-
-	firstMouse = true;
 }
 
 void Camera::changePosition(const float x, const float y) {
+	//set z value
 	float z = 0.02f;
 	if(g_level->gamemode == THIRD_PERSON) z = 0.6f;
+	//update view matrix position value
 	camPos = glm::vec3(x + xOffset, y + yOffset, z);
-
-	viewMatrix = glm::mat4(1.f);
+	//update view matrix
 	viewMatrix = glm::lookAt(camPos, camPos + camFront, camUp);
 }
 
 void Camera::changeDirection(const int pacmanDirection) {
-	direction = pacmanDirection;
+	//set direction value
+	const int direction = pacmanDirection;
 	//reset offset values
 	xOffset = 0.f;
 	yOffset = 0.f;
 	if(g_level->gamemode == THIRD_PERSON) {
+		//rotate camera
 		yaw = 90.f;
 		pitch = 150.f;
 		//default world orientation
 		camUp = glm::vec3(0.f, 1.f, 0.f);
+		//set offset
 		yOffset = -g_level->gridElementWidth * 6.f;
 	} else {
 		switch (direction) {
--- header/camera.h
+++ header/camera.h
@@ -1,10 +1,7 @@
 #ifndef __CAMERA_H
 #define __CAMERA_H
 /* library */
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 /**
  * @brief Leaf class
  * 
@@ -12,14 +9,10 @@
 class Camera {
     private:
         /* private data */
-        float fov, nearPlane, farPlane;
-        float yaw, pitch, lastX, lastY, camSpeed, xOffset = 0, yOffset = 0;
-        bool firstMouse;
+        float yaw, pitch, xOffset = 0, yOffset = 0;
         glm::vec3 camFront, camPos, camUp;
-        const float sensitivity = 0.05f;
     public:
         /* public data */
-        int direction = 0;
         glm::mat4 viewMatrix, projectionMatrix;
         /* public functionality */
         ~Camera();
